827f70841812031660b020cd96ae51f8d2b4a8a2
Eetu Eskelinen <edwardtehgreat@gmail.com>
2017-01-21 13:50:29 +0200

    Added a platform spawner :pig:

--- GGJ17/Assets/Scripts/PlatformSpawner.cs
+++ GGJ17/Assets/Scripts/PlatformSpawner.cs
@@ -3,15 +3,40 @@ using System.Collections;
 
 public class PlatformSpawner : MonoBehaviour 
 {
-	public GameObject[] platforms;
-	public float spawnTime;
+	public GameObject[] platformPrefabs;
+	public Transform spawnLocation;
+	public float maxTime = 2f;
+	public float minTime = 0.5f;
+	public float spawnTime = 3f;
+	private float time;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		InvokeRepeating ("spawnPlatform", spawnTime, spawnTime);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () 
+	{
+		time += Time.deltaTime;
+
+		if (time >= spawnTime) 
+		{
+			spawnPlatform ();
+		}
+
 	}
+
+	void spawnPlatform()
+	{
+		time = 0f;
+		GameObject platform = Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)], spawnLocation.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+
+	//void SetRandomTime()
+	//{
+	//	spawnTime = Random.Range(minTime, maxTime);
+	//}
+
 }
